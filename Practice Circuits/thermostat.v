33ffbe2aba27165a91e34bd24dd80f03ef4e9319
Tyler McClure <tylermcclure11@gmail.com>
2026-05-25 20:51:06 -0400

    thermostat circuit

---
+++ Practice Circuits/thermostat.v
@@ -0,0 +1,15 @@
+module thermostat (
+    input too_cold,
+    input too_hot,
+    input mode,
+    input fan_on,
+    output heater,
+    output aircon,
+    output fan
+);
+
+    assign heater = (mode & too_cold);
+    assign aircon = (too_hot & ~mode);
+    assign fan = (fan_on) | (heater) | (aircon);
+
+endmodule
