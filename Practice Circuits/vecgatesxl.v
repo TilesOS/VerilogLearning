1a9fe691d38248d070cd988469cb51764b717dc6
Tyler McClure <tylermcclure11@gmail.com>
2026-05-30 17:14:29 -0400

    vecgatesxl circuit which shows how a 100 bit vector is no harder to write than a 4 bit vector

--- Practice Circuits/vecgatesxl.v
+++ Practice Circuits/vecgatesxl.v
@@ -7,6 +7,6 @@ module vecgatesxl(
 
     assign out_both = in[98:0] & in[99:1];
     assign out_any = in[98:0] | in[99:1];
-    assign out_different = in ^ {in[0] & in[99:1]}
+    assign out_different = in ^ {in[0] & in[99:1]};
 
 endmodule
