db38b97c5a792287abb2a833d9ac0fc6a717a94b
Tyler McClure <tylermcclure11@gmail.com>
2026-06-02 11:28:43 -0400

    connecting three submodules together and to a top module

---
+++ Verilog Language/Modules/threemodules.v
@@ -0,0 +1,14 @@
+module threemodules (
+    input clk,
+    input d,
+    output q
+);
+
+    wire w1;
+    wire w2;
+
+    my_dff instance1 (.clk(clk), .d(d), .q(w1));
+    my_dff instance2 (.clk(clk), .d(w1), .q(w2));
+    my_dff instance3 (.clk(clk), .d(w2), .q(q));
+
+endmodule
