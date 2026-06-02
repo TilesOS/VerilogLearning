57215282e1870726286b08dfb6c39999194dcb21
Tyler McClure <tylermcclure11@gmail.com>
2026-06-02 11:17:45 -0400

    connecting module ports by name

---
+++ Verilog Language/Modules/portsbyname.v
@@ -0,0 +1,12 @@
+module portsbyname (
+    input a,
+    input b,
+    input c,
+    input d,
+    output out1,
+    output out2
+);
+
+    mod_a instance1 (.out1(out1), .out2(out2), .in1(a), .in2(b), .in3(c), .in4(d));
+
+endmodule
