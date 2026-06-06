baeb583120f82c0e2443df4c8c1e55edf8c9ad1c
Tyler McClure <tylermcclure11@gmail.com>
2026-06-06 10:06:46 -0400

    D latch circuit

---
+++ Practice Circuits/Sequential Logic/Latches and Flip-Flops/Dlatch.v
@@ -0,0 +1,14 @@
+module Dlatch (
+    input d,
+    input ena,
+    output q
+);
+
+    always @(*) begin
+        if (ena)
+            q <= d;
+        else
+            q <= q;
+    end
+
+endmodule
